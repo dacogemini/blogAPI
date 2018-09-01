51551e3689a4212e4268bc4510e923278d30b3d6
Dan Collins <dcollins31@gmail.com>
2018-08-31 20:51:32 -0500

    added Mongoose

--- db/connect.js
+++ db/connect.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 
 const connectToDb = async() => {
     try {
-        await mongoose.connect(config.mongoUrl), { useMongoClient: true });
+        await mongoose.connect(config.mongoUrl), { useMongoClient: true };
     }
     catch(err) {
         console.log(err);
